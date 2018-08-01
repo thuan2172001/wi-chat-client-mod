c4b38b875b42bafa3e23dfa1c7b4b236c0444ce3
bmq <bmq928@gmail.com>
2018-08-02 00:46:39 +0700

    fit text on chat

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -15,18 +15,42 @@ function controller($sce) {
         preProcess()
     }
 
+    self.$onChanges = function(obj) {
+        // console.log({obj})
+        if(obj.type && obj.type.currentValue) self.type = obj.type.currentValue
+        preProcess()
+    }
+
     function preProcess() {
-        
+
         self.isImage = self.type === 'image'
-        if(self.type === 'text')
+        if (self.type === 'text')
             self.text = breakLineText(self.text)
     }
 
     function breakLineText(str) {
-        const MAX_STR_LENGTH = 65
+        const MAX_STR_LENGTH = 59
         const SPECIAL_CHAR = [':', '|', '8']
         const br = '<br>'
-        const strLen = str.length
+        // const STR_LEN = str.length
+
+
+        let start = MAX_STR_LENGTH - 1
+        // console.log({str})
+        while (start < str.length) {
+
+            if (isAlphabetOrDigit(str[start]) || (!isAlphabetOrDigit(str[start]) && isAlphabetOrDigit(str[start + 1]))) {
+
+                const before = str.substring(0, start)
+                const after = str.substring(start, str.length)
+                // console.log({before, after})
+                str = before + br + after
+                start += MAX_STR_LENGTH + br.length
+            } else {
+                ++start
+            }
+
+        }
 
         return str
     }
@@ -34,7 +58,7 @@ function controller($sce) {
     function isAlphabetOrDigit(c) {
         return /^([A-Z]|[0-9])$/i.test(c)
     }
-    
+
 }
 
 export default {
@@ -44,7 +68,7 @@ export default {
             avatar: '<',
             text: '<',
             time: '<',
-            isSentMesg:'<',
+            isSentMesg: '<',
             type: '<'
         },
         template,
