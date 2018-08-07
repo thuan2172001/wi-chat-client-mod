b8ca09570f0fbcb349318d080ee74f9afeb37dc7
bmq <bmq928@gmail.com>
2018-08-07 15:09:34 +0700

    fix ui

---
+++ public/main.cb50a98a7fda54f27c9f.hot-update.js
@@ -0,0 +1,26 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/pages/login/avatar_2x.png":
+/*!***************************************!*\
+  !*** ./src/pages/login/avatar_2x.png ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "avatar_2x.png";
+
+/***/ }),
+
+/***/ "./src/pages/login/login.html":
+/*!************************************!*\
+  !*** ./src/pages/login/login.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = "<div class=login> <div class=container> <div class=\"card card-container\"> <img id=profile-img class=profile-img-card src=" + __webpack_require__(/*! ./avatar_2x.png */ "./src/pages/login/avatar_2x.png") + " /> <p id=profile-name class=profile-name-card></p> <form class=form-signin> <span class=text-danger ng-bind=self.errorMesg></span> <input type=text ng-model=self.username class=form-control placeholder=Username autofocus> <input type=password ng-model=self.password class=form-control placeholder=Password> <button class=\"btn btn-lg btn-primary btn-block btn-signin\" type=submit ng-click=self.submit()>Sign in</button> </form> </div> </div> </div>";
+
+/***/ })
+
+})
+//# sourceMappingURL=main.cb50a98a7fda54f27c9f.hot-update.js.map
