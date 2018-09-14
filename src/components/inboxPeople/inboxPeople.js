8d83ffc978add082db8f373aafdec58045996a20
bmq <bmq928@gmail.com>
2018-09-14 16:31:01 +0700

    add color to label

--- src/components/inboxPeople/inboxPeople.js
+++ src/components/inboxPeople/inboxPeople.js
@@ -7,39 +7,43 @@ controller.$inject = ['dict']
 function controller(dict) {
     const self = this
     let type = 'text'
-    
-    self.$onInit = function() {
+
+    self.$onInit = function () {
+
+        self.company = ''
+        self.labelColor = ''
+
         dict.usernameToCompany(self.showName(), (name) => {
             self.company = name
-            console.log({name})
+            self.labelColor = strToColor(name)
         })
     }
 
-    self.$onChanges = function({msgType}) {
-        if(msgType) type = msgType.currentValue
+    self.$onChanges = function ({ msgType }) {
+        if (msgType) type = msgType.currentValue
     }
-    
-    self.showName = function() {
+
+    self.showName = function () {
         const prefix = 'Help_Desk-'
 
         return self.peopleName.replace(prefix, '')
     }
-    
-    self.showMsg = function() {
-        if(type === 'text') return displayTextMsg()
-        if(type === 'image') return displayImageMsg()
-        if(type === 'file') return displayFileMsg()
+
+    self.showMsg = function () {
+        if (type === 'text') return displayTextMsg()
+        if (type === 'image') return displayImageMsg()
+        if (type === 'file') return displayFileMsg()
     }
 
     function limitStrLen(str, len) {
         const MIN = 3
-        if(len < 3) len = 3
+        if (len < 3) len = 3
 
-        if(str.length > len) return str.substr(0, len - 3) + '...'
-        
+        if (str.length > len) return str.substr(0, len - 3) + '...'
+
         return str
     }
-    
+
     function displayTextMsg() {
         const text = self.latestMesg || ''
         const br1 = '&ltbr&gt'
@@ -52,9 +56,9 @@ function controller(dict) {
         const i2 = text.lastIndexOf(br2)
         const i3 = text.lastIndexOf(br3)
         let i = Math.max(i1, i2, i3)
-        if(i === -1) i = 0
+        if (i === -1) i = 0
         // //console.log({latestMesg : self.latestMesg})
-        
+
         // if(/\n/.test(text)) //console.log({i1, i2, i, 'str': text.substr(i)})
         ////console.log({text: text.substr(i).replace(new RegExp(`(${br1}|${br2}|${br3})`), '')})
         const result = text.substr(i).replace(new RegExp(`(${br1}|${br2}|${br3})`), '')
@@ -68,6 +72,16 @@ function controller(dict) {
     function displayFileMsg() {
         return 'Tin nhắn là một file'
     }
+
+    function strToColor(str) {
+        const colors = ['lightskyblue', 'coral', 'darkcyan', 'burlywood', 'cadetblue',
+            'darkgray', 'darkkhaki', 'darksalmon', 'darkseagrenn', 'darkviolet', 'deepskyblue']
+
+        // convert str to a suitable num
+        const num = str.split('').reduce((acc, cur) => acc + Math.pow(cur.charCodeAt(0), 2), 0)
+        return colors[num % colors.length]
+        
+    }
 }
 
 export default {
@@ -77,9 +91,9 @@ export default {
             isChosen: '<',
             peopleName: '<',
             latestMesg: '<',
-            time:'<',
+            time: '<',
             isSeenYet: '<',
-            msgType:'<'
+            msgType: '<'
         },
         template,
         controller,
