084c02e29f5070a5f9929fafeea75b0d77b7fea6
Quang LN <quangln2810@gmail.com>
2020-07-21 16:49:37 +0700

    fix image

--- src/HOCs/chatContent/chatContent.js
+++ src/HOCs/chatContent/chatContent.js
@@ -22,17 +22,17 @@ function controller(auth) {
     self.isFile = () => self.type === FILE
 
     self.imgThumb = () =>  {
-        let p = self.content.slice(lengthUrl+1);
+        let p = self.content.slice(lengthUrl);
         return ROOT + '/api/thumb/'+p+'?token='+token
     }
 
     self.imgDownload = () => {
-        let p = self.content.slice(lengthUrl+1);
+        let p = self.content.slice(lengthUrl);
         return ROOT +'/api/download/'+p+'?token='+token
     }
 
     self.imgGetImageOrigin = () => {
-        let p = self.content.slice(lengthUrl+1);
+        let p = self.content.slice(lengthUrl);
         return ROOT + '/api/imageOrigin/'+p+'?token='+token;
     }
 
